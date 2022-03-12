f3332ba0bfc17de23adea9ec0eae4a4cb20f976d
Mahir Büyükkal <mahirbuyukkal3@gmail.com>
2022-03-12 16:02:24 +0300

    frist commit

---
+++ multiThread/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btny = new System.Windows.Forms.Button();
+            this.btnb = new System.Windows.Forms.Button();
+            this.btnr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btny
+            // 
+            this.btny.Location = new System.Drawing.Point(103, 185);
+            this.btny.Name = "btny";
+            this.btny.Size = new System.Drawing.Size(75, 23);
+            this.btny.TabIndex = 0;
+            this.btny.Text = "Yellow";
+            this.btny.UseVisualStyleBackColor = true;
+            // 
+            // btnb
+            // 
+            this.btnb.Location = new System.Drawing.Point(238, 185);
+            this.btnb.Name = "btnb";
+            this.btnb.Size = new System.Drawing.Size(75, 23);
+            this.btnb.TabIndex = 1;
+            this.btnb.Text = "Blue";
+            this.btnb.UseVisualStyleBackColor = true;
+            // 
+            // btnr
+            // 
+            this.btnr.Location = new System.Drawing.Point(370, 185);
+            this.btnr.Name = "btnr";
+            this.btnr.Size = new System.Drawing.Size(75, 23);
+            this.btnr.TabIndex = 2;
+            this.btnr.Text = "Red";
+            this.btnr.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 372);
+            this.Controls.Add(this.btnr);
+            this.Controls.Add(this.btnb);
+            this.Controls.Add(this.btny);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btny;
+        private System.Windows.Forms.Button btnb;
+        private System.Windows.Forms.Button btnr;
+    }
+}
+
---
+++ multiThread/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            btnb.Click += Btnb_Click;
+            btny.Click += Btny_Click;
+            btnr.Click += Btnr_Click;
+        }
+        Random rnd;
+        Thread th1;
+        Thread th2;
+        Thread th3;
+
+        private void Btnr_Click(object sender, EventArgs e)
+        {
+            th3 = new Thread(metot3);
+            th3.Start();
+        }
+
+        private void Btny_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(metot1);
+            th1.Start();
+        }
+
+        private void Btnb_Click(object sender, EventArgs e)
+        {
+            th2 = new Thread(metot2);
+            th2.Start();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rnd = new Random();
+        }
+
+        public void metot1() //Yellow
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Yellow, 4),
+                                                    new Rectangle(rnd.Next(0, this.Width),
+                                                    rnd.Next(0, this.Height), 30, 30));
+                Thread.Sleep(100);
+            }
+        }
+        public void metot2() //Blue
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 4),
+                                                    new Rectangle(rnd.Next(0, this.Width),
+                                                    rnd.Next(0, this.Height), 30, 30));
+                Thread.Sleep(100);
+            }
+        }
+        public void metot3() //Red
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 4),
+                                                    new Rectangle(rnd.Next(0, this.Width),
+                                                    rnd.Next(0, this.Height), 30, 30));
+                Thread.Sleep(100);
+            }
+        }
+
+    }
+}
