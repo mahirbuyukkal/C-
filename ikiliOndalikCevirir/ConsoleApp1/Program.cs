6f6b2769c2468ac6f6067f3409bb7afbd6a345eb
Mahir <67606432+mahirbuyukkal@users.noreply.github.com>
2022-05-09 18:07:00 +0300

    Add files via upload

---
+++ ikiliOndalikCevirir/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("ondalık bir sayı giriniz: ");
+            int ond = int.Parse(Console.ReadLine());
+
+            int kalan = 0;
+
+            while (ond > 0)
+            {
+                kalan = ond % 2;
+                Console.Write(kalan);
+                ond /= 2;
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
